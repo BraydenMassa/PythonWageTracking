3f56f610f5d5b527f250abdb52441c5d79a7a014
brayden-massa-snhu <brayden.massa@snhu.edu>
2025-02-14 06:11:07 -0500

    testing git

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
-
-
-###
-
-
-
-
-###
-
-
-
-###
-
-
-
-###
-
-
-###
-
+from App import App
 
 DB_URL = "mongodb+srv://bmassa56:Brayj3156@cluster0.pb0d0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 if __name__ == '__main__':
     App(DB_URL)
+
+# testing
